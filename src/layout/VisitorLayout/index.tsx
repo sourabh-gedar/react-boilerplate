2cce692a3b16ae2aa1a818742340dc4e0bde34c2
sourabh-gedar <sourabhgedar30@gmail.com>
2024-06-15 18:23:58 +0530

    addition : (global path config + layout folder)

---
+++ src/layout/VisitorLayout/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LayoutProps } from '@schemas';
+
+const Index = ({ children }: LayoutProps) => {
+    return <React.Fragment>
+        {children}
+    </React.Fragment>
+}
+
+export default Index
